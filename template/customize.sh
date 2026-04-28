7ba84bf222b2be08c2c1792b8419db6ec22964d2
Timeline <53483352+Nep-Timeline@users.noreply.github.com>
2026-04-28 17:55:06 +0800

    Refactor customize.sh for kernel version handling

--- template/customize.sh
+++ template/customize.sh
@@ -1,47 +1,33 @@
 SKIPUNZIP=0
+
+set_perm_recursive "$MODPATH"/kmod 0 0 0755 0755
+
 KERNEL_VERSION=$(uname -r)
-ui_print "- 当前系统内核: $KERNEL_VERSION"
+ui_print "- 当前内核版本: $KERNEL_VERSION"
 
 CORE_VER=${KERNEL_VERSION%%-*}
 CORE_VER=${CORE_VER%.*}
 
-AND_VER=${KERNEL_VERSION#*-android}
-AND_VER=${AND_VER%%-*}
+if [[ "$KERNEL_VERSION" == *"android"* ]]; then
+    AND_VER=${KERNEL_VERSION#*-android}
+    AND_VER=${AND_VER%%-*}
+else
+    [ "$CORE_VER" = "6.12" ] && AND_VER="16"
+    [ "$CORE_VER" = "6.6" ]  && AND_VER="15"
+    [ "$CORE_VER" = "6.1" ]  && AND_VER="14"
+    [ "$CORE_VER" = "5.15" ] && AND_VER="13"
+    [ "$CORE_VER" = "5.10" ] && AND_VER="12"
+fi
 
-SUPPORTED_VERS=""
-for file in "$MODPATH/module"/*_rekernel.ko; do
-    name=${file##*/} 
-    SUPPORTED_VERS="$SUPPORTED_VERS ${name%_rekernel.ko}"
-done
+TARGET_VER="android${AND_VER}-${CORE_VER}"
 
-TEMP_VER="android${AND_VER}-${CORE_VER}"
-
-if [ -f "$MODPATH/module/${TEMP_VER}_rekernel.ko" ] && [ -f "$MODPATH/loader/${TEMP_VER}-lkmloader.ko" ]; then
-    TARGET_VER="$TEMP_VER"
-elif [[ "$CORE_VER" == "5.10" ]]; then
-    TARGET_VER="android12-5.10"
-elif [[ "$CORE_VER" == "5.15" ]]; then
-    TARGET_VER="android13-5.15"
-elif [[ "$CORE_VER" == "6.1" ]]; then
-    TARGET_VER="android14-6.1"
-elif [[ "$CORE_VER" == "6.6" ]]; then
-    TARGET_VER="android15-6.6"
-elif [[ "$CORE_VER" == "6.12" ]]; then
-    TARGET_VER="android16-6.12"
+if ls $MODPATH/kmod/rekernel-${TARGET_VER}* >/dev/null 2>&1; then
+    ui_print "- 自动匹配成功: $TARGET_VER"
 else
     abort "! 自动匹配失败，请查看安装脚本并自行修改"
 fi
 
-ui_print "- 自动匹配成功: $TARGET_VER"
-LKM_SRC="$MODPATH/loader/${TARGET_VER}-lkmloader.ko"
-REK_SRC="$MODPATH/module/${TARGET_VER}_rekernel.ko"
-cp "$LKM_SRC" "$MODPATH/lkm-loader.ko"
-cp "$REK_SRC" "$MODPATH/re-kernel.ko"
+cp -fp "$MODPATH"/kmod/rekernel-${TARGET_VER}* "$MODPATH"/
+rm -rf $MODPATH/kmod
 
-rm -rf $MODPATH/loader
-rm -rf $MODPATH/module
-
-set_perm "$MODPATH/lkm-loader.ko" 0 0 0755
-set_perm "$MODPATH/re-kernel.ko" 0 0 0755
-
-ui_print "- 操作完成！内核端口ID: 100"
+ui_print "- 操作完成！内核端口ID: 100"REK_SRC="$MODPATH/module/${TARGET_VER}_rekernel.ko"
