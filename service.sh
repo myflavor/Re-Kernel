df4b2b603a5267a55f7b4f6ef0f19ac107f9450b
Timeline <53483352+Nep-Timeline@users.noreply.github.com>
2026-04-28 19:48:12 +0800

    Delete service.sh

--- service.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-MODDIR=${0%/*}
-for mod in "$MODDIR"/rekernel-*.ko; do
-    if [ -f "$mod" ]; then
-        if insmod "$mod" >/dev/null 2>&1; then
-            break
-        fi
-    fi
-done
